c1b1a3d8131543057d3ac7545c9825c3375725b6
pawel21 <p.gliwny@gmail.com>
2025-07-08 22:55:29 +0200

    Working on project

--- pipeline_to_test_feature_extractors/feature_extractors.py
+++ pipeline_to_test_feature_extractors/feature_extractors.py
@@ -1,7 +1,6 @@
 import numpy as np
 from skimage.feature import hog
 from sklearn.decomposition import PCA
-from skimage.feature import local_binary_pattern
 
 
 def raw_pixels(image):
@@ -10,10 +9,6 @@ def raw_pixels(image):
 def hog_features(image):
     return hog(image, pixels_per_cell=(4, 4), cells_per_block=(2, 2), feature_vector=True)
 
-def lbp_histogram(image, P=8, R=1):
-    lbp = local_binary_pattern(image, P, R, method='uniform')
-    hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, P + 3), density=True)
-    return hist
 
 def pca_features(images, n_components=10):
     flat = [img.flatten() / 255.0 for img in images]
