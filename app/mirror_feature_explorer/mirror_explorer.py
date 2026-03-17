7a98edfe0f94482a3a949e7ee6551394553c6e37
pawel21 <p.gliwny@gmail.com>
2026-03-17 10:23:04 +0100

    Work on baseline

--- app/mirror_feature_explorer/mirror_explorer.py
+++ app/mirror_feature_explorer/mirror_explorer.py
@@ -2,6 +2,11 @@ import numpy as np
 import streamlit as st
 import plotly.graph_objects as go
 
+from PIL import Image, ImageDraw
+
+def render_image(img):
+    st.image(img, caption="MAGIC I telescope", width=400)
+
 st.set_page_config(
     page_title="Mirror Feature Explorer",
     layout="wide",
@@ -16,11 +21,30 @@ def load_data(path):
     feat_names = data['feature_names']
     return feat_array, feat_names
 
-fm, feat_names = load_data("data/feature_matrix_1.npz")
+@st.cache_data
+def load_img(path):
+    img = Image.open(path)
+    return img
+
+fm, feat_names = load_data("../../data/feature_matrix_1.npz")
+img = load_img("data/IRCamM1T20250915_163000M.jpg")
+
+feature_names = ['lbp_entropy', 'glcm_contrast', 'glcm_dissimilarity',
+                 'glcm_homogeneity', 'glcm_energy', 'glcm_correlation',
+                 "sobel_mean", "soble_max",
+                 "laplacian_mean", "laplacian_std", "edge_density"]
+
+feat_names_dict = {}
+i = 0
+for name in feature_names:
+    feat_names_dict[name] = i
+    i += 1
+
+print(feat_names_dict)
 
 T, N_mirrors, N_feats = fm.shape
 st.write(f"Dane: {T} zdjęć, {N_mirrors} luster, {N_feats} features")
-st.write(feat_names)
+
 
 with st.sidebar:
     st.header("Settings")
@@ -39,19 +63,22 @@ with st.sidebar:
     )
 
     st.divider()
-    st.write(f"Mirror: {selected_mirror}, Feature: {selected_feature}")
+    st.write(f"Mirror: {selected_mirror}, Feature: {selected_feature}, feat id: {feat_names_dict[selected_feature]}")
 
 # Wykres
 st.subheader(f"Feature: {selected_feature}")
 
+render_image(img)
+
 fig = go.Figure()
+feat_id = feat_names_dict[selected_feature]
 
 for mirror_id in selected_mirror:
-    series = fm[:, mirror_id, 0]
+    series = fm[:, mirror_id, feat_id]
 
     fig.add_trace(go.Scatter(
         x=list(range(T)),
-        y=fm[:, mirror_id, 1],
+        y=series,
         mode = "lines+markers",
         name = mirror_id,
         marker=dict(size=4),
