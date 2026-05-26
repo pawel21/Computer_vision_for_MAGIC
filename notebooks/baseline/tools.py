625c21bb569003085647dde0c7af195c215ea5f1
pawel21 <p.gliwny@gmail.com>
2026-05-26 23:01:37 +0200

    Create img which show segmentation

--- notebooks/baseline/tools.py
+++ notebooks/baseline/tools.py
@@ -4,33 +4,11 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-def mark_mirrors_on_img(img_path, points_list):
+
+def mark_mirrors_on_img(img_path, points_list, color, ax):
     img_bgr = cv2.imread(str(img_path))
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
-    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-    ax.imshow(img_rgb)
-
-    for points in points_list:
-        # === Polygon na podstawie 4 współrzędnych ===
-        polygon = patches.Polygon(
-                points,
-                closed=True,
-                linewidth=2,
-                edgecolor='red',
-                facecolor='red',
-                alpha=0.3  # przezroczystość wypełnienia
-            )
-        ax.add_patch(polygon)
-
-    plt.tight_layout()
-    plt.show()
-
-def mark_mirrors_on_img(img_path, points_list, color):
-    img_bgr = cv2.imread(str(img_path))
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-
-    fig, ax = plt.subplots(1, 1, figsize=(8, 4))
     ax.imshow(img_rgb)
 
     for points in points_list:
@@ -44,6 +22,3 @@ def mark_mirrors_on_img(img_path, points_list, color):
                 alpha=0.3  # przezroczystość wypełnienia
             )
         ax.add_patch(polygon)
-
-    plt.tight_layout()
-    plt.show()
